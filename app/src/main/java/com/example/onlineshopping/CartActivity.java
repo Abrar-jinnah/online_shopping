e6d0ae58644fd5cf7cac513f330d0c7c5be96f35
Abrar-jinnah <abrarjinnah@gmail.com>
2020-10-03 21:51:46 +0530

    cart activty javaa

--- app/src/main/java/com/example/onlineshopping/CartActivity.java
+++ app/src/main/java/com/example/onlineshopping/CartActivity.java
@@ -27,7 +27,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import Model.Cart;
-import Prevalent.CartViewHolder;
+import ViewHolder.CartViewHolder;
 import Prevalent.Prevalent;
 
 public class CartActivity extends AppCompatActivity {
