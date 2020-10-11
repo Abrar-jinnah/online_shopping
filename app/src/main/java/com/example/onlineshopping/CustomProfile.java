20d43c85a65d06e859e3d6c06f7b1a436e80deda
Harshana-98 <68683395+Harshana-98@users.noreply.github.com>
2020-10-11 23:22:45 +0530

    fix CustomProfile

--- app/src/main/java/com/example/onlineshopping/CustomProfile.java
+++ app/src/main/java/com/example/onlineshopping/CustomProfile.java
@@ -21,7 +21,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.StorageTask;
 import com.squareup.picasso.Picasso;
 import com.theartofdev.edmodo.cropper.CropImage;
 
@@ -33,7 +32,9 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class CustomProfile extends AppCompatActivity {
 
     private CircleImageView profileImageView;
-    private EditText userNameEditText, userPhoneEditText, passwordEditText;
+    private EditText userNameEditText;
+    private EditText userPhoneEditText;
+    private EditText passwordEditText;
     private Uri imageUri;
     private String myUrl = "";
     private StorageReference storageProfilePictureRef;
